82744cdd8781f048c9176cf25530117a235f669b
kt1313 <tomaklim@wp.pl>
2020-12-14 14:20:59 +0100

    VisitCounter - działa, zeruje?

--- src/main/java/pl/javastart/cookies/RemoveController.java
+++ src/main/java/pl/javastart/cookies/RemoveController.java
@@ -1,4 +1,17 @@
 package pl.javastart.cookies;
 
-public class RemoveController {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+
+import java.io.IOException;
+
+@WebServlet("/remove")
+public class RemoveController extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Cookie removeVisitCookie = new Cookie("visits", "");
+        removeVisitCookie.setMaxAge(0);
+        response.addCookie(removeVisitCookie);
+        request.getRequestDispatcher("/removeConfirm.jsp").forward(request, response);
+    }
 }
--- src/main/webapp/removeConfirm.jsp
+++ src/main/webapp/removeConfirm.jsp
@@ -2,17 +2,15 @@
   Created by IntelliJ IDEA.
   User: Dell
   Date: 2020-12-14
-  Time: 13:44
+  Time: 14:18
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Wizyty</title>
+    <title>Title</title>
 </head>
 <body>
-<h1>Cześć!</h1>
-<h2>Twoja liczba odwiedzin na naszej stronie: <%= request.getAttribute("numberOfVisits")%></h2>
-<a href="remove">Wyzeruj licznik</a>
+
 </body>
 </html>
